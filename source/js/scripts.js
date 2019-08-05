fa4d64fa62e0a7a90a097b9300189911f72b9138
Egor Melnichenko <egor.melnichenko@bk.ru>
2019-08-05 16:32:44 +0300

    index-tablet соответствует макету

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -27,11 +27,16 @@ buttonBefore.addEventListener('click', () => {
   if(imageBefore.classList.contains('real-story__image--active')) return;
   imageAfter.classList.remove('real-story__image--active');
   imageBefore.classList.add('real-story__image--active');
+  imageToggle.classList.remove('slider__toggle--right');
+  imageToggle.classList.add('slider__toggle--left');
+
 });
 
 buttonAfter.addEventListener('click', () => {
   if(imageAfter.classList.contains('real-story__image--active')) return;
   imageBefore.classList.remove('real-story__image--active');
   imageAfter.classList.add('real-story__image--active');
+  imageToggle.classList.remove('slider__toggle--left');
+  imageToggle.classList.add('slider__toggle--right');
 });
 
