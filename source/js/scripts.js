45a3c222183dc41138131b47879a587b3e4fcc58
Egor Melnichenko <egor.melnichenko@bk.ru>
2019-08-05 13:45:07 +0300

    index-mobile соответствует макету

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,8 +2,14 @@
 
 const mainNav = document.querySelector('.main-nav');
 const menuToggle = document.querySelector('.main-nav__toggle');
+const buttonBefore = document.querySelector('.slider__button-before');
+const buttonAfter = document.querySelector('.slider__button-after');
+const imageBefore = document.querySelector('.real-story__image--before');
+const imageAfter = document.querySelector('.real-story__image--after');
+const imageToggle = document.querySelector('.slider__toggle');
 
 mainNav.classList.remove('main-nav--nojs');
+
 
 menuToggle.addEventListener('click', () => {
 
@@ -16,3 +22,16 @@ menuToggle.addEventListener('click', () => {
   mainNav.classList.remove('main-nav--opened');
   mainNav.classList.add('main-nav--closed');
 })
+
+buttonBefore.addEventListener('click', () => {
+  if(imageBefore.classList.contains('real-story__image--active')) return;
+  imageAfter.classList.remove('real-story__image--active');
+  imageBefore.classList.add('real-story__image--active');
+});
+
+buttonAfter.addEventListener('click', () => {
+  if(imageAfter.classList.contains('real-story__image--active')) return;
+  imageBefore.classList.remove('real-story__image--active');
+  imageAfter.classList.add('real-story__image--active');
+});
+
