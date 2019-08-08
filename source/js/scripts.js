f5bd3e2eb5e2adbe670b0e5cb385e2a0d2bbff2e
Egor Melnichenko <egor.melnichenko@bk.ru>
2019-08-08 16:44:03 +0300

    Страница каталога соответствует макету

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,14 +2,8 @@
 
 const mainNav = document.querySelector('.main-nav');
 const menuToggle = document.querySelector('.main-nav__toggle');
-const buttonBefore = document.querySelector('.slider__button-before');
-const buttonAfter = document.querySelector('.slider__button-after');
-const imageBefore = document.querySelector('.real-story__image--before');
-const imageAfter = document.querySelector('.real-story__image--after');
-const imageToggle = document.querySelector('.slider__toggle');
 
 mainNav.classList.remove('main-nav--nojs');
-
 
 menuToggle.addEventListener('click', () => {
 
@@ -22,21 +16,3 @@ menuToggle.addEventListener('click', () => {
   mainNav.classList.remove('main-nav--opened');
   mainNav.classList.add('main-nav--closed');
 })
-
-buttonBefore.addEventListener('click', () => {
-  if(imageBefore.classList.contains('real-story__image--active')) return;
-  imageAfter.classList.remove('real-story__image--active');
-  imageBefore.classList.add('real-story__image--active');
-  imageToggle.classList.remove('slider__toggle--right');
-  imageToggle.classList.add('slider__toggle--left');
-
-});
-
-buttonAfter.addEventListener('click', () => {
-  if(imageAfter.classList.contains('real-story__image--active')) return;
-  imageBefore.classList.remove('real-story__image--active');
-  imageAfter.classList.add('real-story__image--active');
-  imageToggle.classList.remove('slider__toggle--left');
-  imageToggle.classList.add('slider__toggle--right');
-});
-
